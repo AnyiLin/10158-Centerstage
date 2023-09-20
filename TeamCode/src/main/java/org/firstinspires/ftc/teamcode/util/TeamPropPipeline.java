10a9b0be0db1e1ed29fa7e6ce4ef1872e60a9d9d
brotherhobo <anyo3223@gmail.com>
2023-09-19 21:59:00 -0400

    small changes to opencv team prop detection pipeline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/TeamPropPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/TeamPropPipeline.java
@@ -10,16 +10,32 @@ public class TeamPropPipeline extends OpenCvPipeline {
 
     private Mat output, hsv;
 
-    private int width, grayError;
+    private final int WIDTH, HEIGHT, GRAY_ERROR, COLOR;
+
+    private int leftTotal, middleTotal, rightTotal;
+
+    private String navigation;
 
     public TeamPropPipeline() {
-        width = 4;
-        grayError = 20;
+        WIDTH = 4;
+        HEIGHT = 80;
+        GRAY_ERROR = 20;
+        COLOR = 0;
     }
 
-    public TeamPropPipeline(int width, int grayError) {
-        this.width = width;
-        this.grayError = grayError;
+    public TeamPropPipeline(int color) {
+        WIDTH = 4;
+        HEIGHT = 80;
+        GRAY_ERROR = 20;
+        this.COLOR = color;
+    }
+
+    // color corresponds with RGB values, with 0 being red, 1 being green, and 2 being blue
+    public TeamPropPipeline(int WIDTH, int height, int grayError, int color) {
+        this.WIDTH = WIDTH;
+        this.HEIGHT = height;
+        this.GRAY_ERROR = grayError;
+        this.COLOR = color;
     }
 
     @Override
@@ -29,54 +45,56 @@ public class TeamPropPipeline extends OpenCvPipeline {
         hsv = output.clone();
         Imgproc.cvtColor(output, hsv, Imgproc.COLOR_RGB2HSV);
 
-        int leftTotal = 0;
-
         //left column
-        for (int counter = 0; counter < output.size().height; counter++) {
-            for (int counter2 = 0; counter2 < width; counter2++) {
-                if (!(hsv.get(counter, counter2)[1]<grayError))
-                    leftTotal += (output.get(counter, counter2)[0]);
+        leftTotal = 0;
+        for (int counter = output.height()- HEIGHT; counter < output.height(); counter++) {
+            for (int counter2 = 0; counter2 < WIDTH; counter2++) {
+                if (!(hsv.get(counter, counter2)[1]< GRAY_ERROR))
+                    leftTotal += (output.get(counter, counter2)[COLOR]);
             }
         }
-        // writes left column total
-        Imgproc.putText(output, leftTotal+"", new Point(0,10), Imgproc.FONT_HERSHEY_COMPLEX, 0.5, new Scalar(0,0,255),1);
-
-        int middleTotal = 0;
 
         //middle column
-        for (int counter = 0; counter < output.height(); counter++) {
-            for (int counter2 = (int)(output.width()/2.0-width/2.0); counter2 < (int)(output.width()/2.0+width/2.0); counter2++) {
-                if (!(hsv.get(counter, counter2)[1]<grayError))
-                    middleTotal += (output.get(counter, counter2)[0]);
+        middleTotal = 0;
+        for (int counter = output.height()- HEIGHT; counter < output.height(); counter++) {
+            for (int counter2 = (int)(output.width()/2.0- WIDTH /2.0); counter2 < (int)(output.width()/2.0+ WIDTH /2.0); counter2++) {
+                if (!(hsv.get(counter, counter2)[1]< GRAY_ERROR))
+                    middleTotal += (output.get(counter, counter2)[COLOR]);
             }
         }
-        // writes middle column total
-        Imgproc.putText(output, middleTotal+"", new Point(output.width()/2,10), Imgproc.FONT_HERSHEY_COMPLEX, 0.5, new Scalar(0,0,255),1);
-
-        int rightTotal = 0;
 
         //right column
-        for (int counter = 0; counter < output.height(); counter++) {
-            for (int counter2 = (int)(output.width()-width); counter2 < output.width(); counter2++) {
-                if (!(hsv.get(counter, counter2)[1]<grayError))
-                    rightTotal += (output.get(counter, counter2)[0]);
+        rightTotal = 0;
+        for (int counter = output.height()- HEIGHT; counter < output.height(); counter++) {
+            for (int counter2 = (int)(output.width()- WIDTH); counter2 < output.width(); counter2++) {
+                if (!(hsv.get(counter, counter2)[1]< GRAY_ERROR))
+                    rightTotal += (output.get(counter, counter2)[COLOR]);
             }
         }
 
-        // writes middle column total
-        Imgproc.putText(output, rightTotal+"", new Point(output.width()-70,10), Imgproc.FONT_HERSHEY_COMPLEX, 0.5, new Scalar(0,0,255),1);
-
-
-
-        // writes which area is the highest
-        String navigation = "";
         if (leftTotal == Math.max(leftTotal, Math.max(middleTotal, rightTotal))) navigation = "left";
         if (middleTotal == Math.max(leftTotal, Math.max(middleTotal, rightTotal))) navigation = "middle";
         if (rightTotal == Math.max(leftTotal, Math.max(middleTotal, rightTotal))) navigation = "right";
-        Imgproc.putText(output, navigation, new Point(output.width()/2,output.height()-30), Imgproc.FONT_HERSHEY_COMPLEX, 0.5, new Scalar(0,0,255),1);
+
+        writeOnScreen();
 
         hsv.release();
 
         return output;
+    }
+
+    public String getNavigation() {
+        return navigation;
+    }
+
+    public void writeOnScreen() {
+        // writes left column total
+        Imgproc.putText(output, leftTotal+"", new Point(0,10), Imgproc.FONT_HERSHEY_COMPLEX, 0.5, new Scalar(0,0,255),1);
+        // writes middle column total
+        Imgproc.putText(output, middleTotal+"", new Point(output.width()/2,10), Imgproc.FONT_HERSHEY_COMPLEX, 0.5, new Scalar(0,0,255),1);
+        // writes right column total
+        Imgproc.putText(output, rightTotal+"", new Point(output.width()-70,10), Imgproc.FONT_HERSHEY_COMPLEX, 0.5, new Scalar(0,0,255),1);
+        // writes which area is the highest
+        Imgproc.putText(output, navigation, new Point(output.width()/2,output.height()-30), Imgproc.FONT_HERSHEY_COMPLEX, 0.5, new Scalar(0,0,255),1);
     }
 }
