557174a190eaa2c167e9beddf74e7364cc20e12f
brotherhobo <anyo3223@gmail.com>
2024-02-13 16:21:46 -0500

    finalized pre-state autos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/notcompetition/teleop/TestBackDropPipelineTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/notcompetition/teleop/TestBackDropPipelineTeleOp.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.notcompetition.teleop;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -10,6 +11,7 @@ import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 
 @TeleOp
+@Disabled
 public class TestBackDropPipelineTeleOp extends LinearOpMode {
     private OpenCvCamera camera;
 
