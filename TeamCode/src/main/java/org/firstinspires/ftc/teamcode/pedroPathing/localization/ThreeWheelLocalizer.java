06b7ab43ac2125b64f16419dbe2cf7e82ff42616
brotherhobo <anyo3223@gmail.com>
2024-03-18 15:22:07 -0400

    adding readmes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/ThreeWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/ThreeWheelLocalizer.java
@@ -8,8 +8,6 @@ import com.acmerobotics.roadrunner.localization.ThreeTrackingWheelLocalizer;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.teamcode.roadrunner.util.Encoder;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -46,7 +44,7 @@ public class ThreeWheelLocalizer extends ThreeTrackingWheelLocalizer {
 
     public static double leftX = -18.5/25.4 - 0.1, leftY = 164.4/25.4, rightX = -18.4/25.4 - 0.1, rightY = -159.6/25.4, strafeX = -107.9/25.4+0.25, strafeY = -1.1/25.4-0.23;
 
-    private Encoder leftEncoder, rightEncoder, strafeEncoder;
+    private RoadRunnerEncoder leftEncoder, rightEncoder, strafeEncoder;
 
     private List<Integer> lastEncPositions, lastEncVels;
 
@@ -61,14 +59,14 @@ public class ThreeWheelLocalizer extends ThreeTrackingWheelLocalizer {
         lastEncVels = lastTrackingEncVels;
 
         // TODO: redo the configs here
-        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "leftRear"));
-        rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "rightFront"));
-        strafeEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "strafeEncoder"));
+        leftEncoder = new RoadRunnerEncoder(hardwareMap.get(DcMotorEx.class, "leftRear"));
+        rightEncoder = new RoadRunnerEncoder(hardwareMap.get(DcMotorEx.class, "rightFront"));
+        strafeEncoder = new RoadRunnerEncoder(hardwareMap.get(DcMotorEx.class, "strafeEncoder"));
 
         // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
-        leftEncoder.setDirection(Encoder.Direction.REVERSE);
-        rightEncoder.setDirection(Encoder.Direction.REVERSE);
-        strafeEncoder.setDirection(Encoder.Direction.FORWARD);
+        leftEncoder.setDirection(RoadRunnerEncoder.Direction.REVERSE);
+        rightEncoder.setDirection(RoadRunnerEncoder.Direction.REVERSE);
+        strafeEncoder.setDirection(RoadRunnerEncoder.Direction.FORWARD);
     }
 
     public void resetHeading(double heading) {
